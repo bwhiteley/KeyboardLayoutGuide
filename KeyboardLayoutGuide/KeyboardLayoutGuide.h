93d78db766c77b5fbde89811d46b97e32317a9d2
Bart Whiteley <@bwhiteley>
2017-11-19 22:40:38 -0700

    added project files

---
+++ KeyboardLayoutGuide/KeyboardLayoutGuide.h
@@ -0,0 +1,19 @@
+//
+//  KeyboardLayoutGuide.h
+//  KeyboardLayoutGuide
+//
+//  Created by J. B. Whiteley on 11/19/17.
+//  Copyright © 2017 SwiftBit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for KeyboardLayoutGuide.
+FOUNDATION_EXPORT double KeyboardLayoutGuideVersionNumber;
+
+//! Project version string for KeyboardLayoutGuide.
+FOUNDATION_EXPORT const unsigned char KeyboardLayoutGuideVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KeyboardLayoutGuide/PublicHeader.h>
+
+
